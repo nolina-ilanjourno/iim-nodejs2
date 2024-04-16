a30e90a477bd088cb0c70610a34433be31a57380
Ilan JOURNO <ilan.journo@nolina.io>
2024-04-16 12:12:45 +0200

    :sparkles: Add some features

--- index.js
+++ index.js
@@ -4,31 +4,72 @@ const port = 3000;
 
 app.use(express.json());
 
-const users = [
+let users = [
   { id: 1, name: "Alice" },
   { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+  { id: 4, name: "David" },
+  { id: 5, name: "Eve" },
+  { id: 6, name: "Fiona" },
 ];
 
 app.get("/users", (req, res) => {
-  return res.send(users);
-});
-
-app.get("/users", (req, res) => {
-  return res.send(variablePublication);
+  return res.status(200).send(users);
 });
 
 app.post("/users", (req, res) => {
   const body = req.body;
-  variablePublication.push(body);
-  return res.send(variablePublication);
+  users.push(body);
+  return res.status(201).send(users);
 });
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id; // 4
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user === undefined) {
+    return res.status(404).send("User not found");
+  }
+
+  return res.status(200).send(user);
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user === undefined) {
+    return res.status(404).send("User not found");
+  }
+
+  user = body;
+
+  return res.status(200).send(user);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user === undefined) {
+    return res.status(404).send("User not found");
+  }
+
+  users = users.filter((user) => user.id !== parseInt(id));
+
+  return res.status(204);
+  // return res.status(200).send(users);
+});
+
+//* /users en GET = tous les utilisateurs
+//* /users en POST = créer un utilisateur
+//* /users/:id en GET = un utilisateur
+//* /users/:id en PUT = modifier un utilisateur
+// /users/:id en DELETE = supprimer un utilisateur
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// /users en GET = tous les utilisateurs
-// /users en POST = créer un utilisateur
-// /users/:id en GET = un utilisateur
-// /users/:id en PUT = modifier un utilisateur
-// /users/:id en DELETE = supprimer un utilisateur
