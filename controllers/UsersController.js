42a3c324770969540542aed91bcfb37b8db0c5f0
Ilan JOURNO <ilan.journo@nolina.io>
2024-04-17 13:05:06 +0200

    :bug: Debug

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,37 @@
-let users = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
-  { id: 4, name: "David" },
-  { id: 5, name: "Eve" },
-  { id: 6, name: "Fiona" },
-];
+const prisma = require("../config/prisma");
 
 class UsersController {
   // app.get (/users)
-  index(req, res) {
+  async index(req, res) {
+    const users = await prisma.user.findMany();
     return res.status(200).send(users);
   }
 
   // app.post (/users)
-  store(req, res) {
-    const body = req.body;
-    users.push(body);
-    return res.status(201).send(users);
+  async store(req, res) {
+    try {
+      const body = req.body;
+      const user = await prisma.user.create({
+        data: body,
+      });
+      return res.status(201).send(user);
+    } catch (e) {
+      return res.status(500).send({
+        message: e.message,
+      });
+    }
   }
 
   // app.get (/users/:id)
-  show(req, res) {
+  async show(req, res) {
     const id = req.params.id; // 4
-    const user = users.find((user) => user.id === parseInt(id));
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
-    if (user === undefined) {
+    if (user === null) {
       return res.status(404).send("User not found");
     }
 
@@ -33,36 +39,65 @@ class UsersController {
   }
 
   // app.put (/users/:id)
-  update(req, res) {
-    const id = req.params.id;
-    const body = req.body;
+  async update(req, res) {
+    try {
+      const id = req.params.id;
+      const body = req.body;
 
-    let user = users.find((user) => user.id === parseInt(id));
+      let user = await prisma.user.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
 
-    if (user === undefined) {
-      return res.status(404).send("User not found");
+      if (user === null) {
+        return res.status(404).send("User not found");
+      }
+
+      user = await prisma.user.update({
+        where: {
+          id: parseInt(id),
+        },
+        data: body,
+      });
+
+      return res.status(200).send(user);
+    } catch (e) {
+      return res.status(500).send({
+        message: e.message,
+      });
     }
-
-    user = body;
-
-    return res.status(200).send(user);
   }
 
   // app.delete (/users/:id)
-  destroy(req, res) {
-    const id = req.params.id;
+  async destroy(req, res) {
+    try {
+      const id = req.params.id;
 
-    const user = users.find((user) => user.id === parseInt(id));
+      let user = await prisma.user.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
 
-    if (user === undefined) {
-      return res.status(404).send("User not found");
+      if (user === null) {
+        return res.status(404).send("User not found");
+      }
+
+      await prisma.user.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+
+      return res.status(204).send();
+      // return res.status(200).send(users);
+    } catch (e) {
+      return res.status(500).send({
+        message: e.message,
+      });
     }
-
-    users = users.filter((user) => user.id !== parseInt(id));
-
-    return res.status(204);
-    // return res.status(200).send(users);
   }
 }
 
-module.exports = UsersController;
+module.exports = new UsersController();
